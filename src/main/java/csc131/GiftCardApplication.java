2654ce1147baf4e99bad2f7a2e30cf0c1358ad52
sang nguyen <nnqsang@gmail.com>
2021-09-30 18:28:18 -0700

    Add the third gift card to gift card collections

--- src/main/java/csc131/GiftCardApplication.java
+++ src/main/java/csc131/GiftCardApplication.java
@@ -7,6 +7,7 @@ public class GiftCardApplication {
 	public static void main(String[] args) {
 		GiftCard gc1 = new GiftCard(25, 125.00);
 		GiftCard gc2 = new GiftCard(50, 150.50);
+		GiftCard gc2 = new GiftCard(75, 175.00);
 		ArrayList<GiftCard> giftCards = new ArrayList<GiftCard>();
 		giftCards.add(gc1);
 		giftCards.add(gc2);
